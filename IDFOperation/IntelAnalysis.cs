b28e23bb5bc65d1310053224da44a1477db3a040
srullsim <simchonisrael@gmail.com>
2025-05-21 12:21:50 +0300

    update menu

--- IDFOperation/IntelAnalysis.cs
+++ IDFOperation/IntelAnalysis.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDFOperation.HAMAS;
+using IDFOperation.IDF;
 
 namespace IDFOperation
 {
     internal class IntelAnalysis
     {
+        public static Terrorist AnalysisTerrorist(List<IntelligenceMessage> intel)
+        {
+            return Terrorist;
+        }
     }
 }
